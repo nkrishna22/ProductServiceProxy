8d83157164b15948300374380f6f0c50debe1e6e
nkrishna22 <ravikrishnanaidu@gmail.com>
2023-10-21 19:40:14 +0530

    initial Spring project created

---
+++ src/test/java/ravi/productserviceproxy/ProductServiceProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package ravi.productserviceproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
